71ac4ec681ead2248ba722cbf9eb665b6a552bfe
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-08-30 17:39:29 +0200

    Add Ember.js addon implementation and basic smoke test

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 /* eslint-env node */
 'use strict';
 
+const path = require('path');
+const Funnel = require('broccoli-funnel');
+const MergeTrees = require('broccoli-merge-trees');
+
 module.exports = {
-  name: 'qunit-dom'
+  name: 'qunit-dom',
+
+  included() {
+    this._super.included.apply(this, arguments);
+    this.import('vendor/qunit-dom.js', { type: 'test' });
+  },
+
+  treeForVendor(vendorTree) {
+    let qunitPluginTree = new Funnel(path.join(this.project.root, 'dist'), {
+      files: ['qunit-dom.js', 'qunit-dom.js.map'],
+    });
+
+    return new MergeTrees([vendorTree, qunitPluginTree]);
+  },
 };
---
+++ tests/acceptance/qunit-dom-test.js
@@ -0,0 +1,16 @@
+import { test } from 'qunit';
+import moduleForAcceptance from '../../tests/helpers/module-for-acceptance';
+
+moduleForAcceptance('Acceptance | qunit-dom');
+
+test('qunit-dom assertions are available', function(assert) {
+  assert.expect(3);
+
+  assert.ok(assert.dom, 'assert.dom is available');
+  assert.ok(assert.dom.textContains, 'assert.dom.textContains is available');
+
+  visit('/');
+  andThen(() => {
+    assert.dom.textContains(find('#title').get(0), 'Welcome');
+  });
+});
