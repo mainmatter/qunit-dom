012bad206da7746333b9a80454675ac03f1857ad
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-10-07 19:36:43 +0200

    Use assert.dom(selector).exists() instead of assert.dom.exists(selector)

--- tests/acceptance/qunit-dom-test.js
+++ tests/acceptance/qunit-dom-test.js
@@ -7,12 +7,12 @@ test('qunit-dom assertions are available', function(assert) {
   assert.expect(5);
 
   assert.ok(assert.dom, 'assert.dom is available');
-  assert.ok(assert.dom.textContains, 'assert.dom.textContains is available');
+  assert.ok(assert.dom('.foo').textContains, 'assert.dom(...).textContains is available');
 
   visit('/');
   andThen(() => {
-    assert.dom.exists('#title');
-    assert.dom.missing('#subtitle');
-    assert.dom.textContains('#title', 'Welcome');
+    assert.dom('#title').exists();
+    assert.dom('#subtitle').missing();
+    assert.dom('#title').textContains('Welcome');
   });
 });
