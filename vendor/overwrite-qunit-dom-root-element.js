9793a9488d4e9038ae5cc98ec623bbeb4df57a73
simon <ihmig@kaliber5.de>
2017-11-28 11:28:08 +0100

    Fix Ember rootElement
    Fixes #39

--- vendor/overwrite-qunit-dom-root-element.js
+++ vendor/overwrite-qunit-dom-root-element.js
@@ -1,6 +1,6 @@
 Object.defineProperty(QUnit.assert.dom, 'rootElement', {
   get: function() {
-    return document.querySelector('#ember-testing');
+    return document.querySelector('#ember-testing > .ember-view') || document.querySelector('#ember-testing');
   },
   enumerable: true,
   configurable: true,
