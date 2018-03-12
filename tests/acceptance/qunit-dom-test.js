f056786526a708a6716692e345558d4b6a7ba749
Lenny Burdette <lenny@squareup.com>
2018-03-12 10:28:08 -0700

    ENHANCEMENT assert.dom() with no argument defaults to rootElement
    Addresses #57

--- tests/acceptance/qunit-dom-test.js
+++ tests/acceptance/qunit-dom-test.js
@@ -4,12 +4,14 @@ import moduleForAcceptance from '../../tests/helpers/module-for-acceptance';
 moduleForAcceptance('Acceptance | qunit-dom');
 
 test('qunit-dom assertions are available', function(assert) {
-  assert.expect(6);
+  assert.expect(7);
 
   assert.ok(assert.dom, 'assert.dom is available');
   assert.ok(assert.dom('.foo').includesText, 'assert.dom(...).includesText is available');
 
   assert.dom('#qunit').doesNotExist('rootElement is set to #ember-testing-container');
+
+  assert.dom().hasAttribute('id', 'ember-testing');
 
   visit('/');
   andThen(() => {
