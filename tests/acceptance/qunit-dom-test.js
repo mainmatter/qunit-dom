9c57b798a76541f7d82f59a70144a6f86fb79455
Steve Calvert <scalvert@linkedin.com>
2018-05-31 08:07:06 -0700

    Fixes is-visible/is-not-visible assertions to correctly handle missing elements

--- tests/acceptance/qunit-dom-test.js
+++ tests/acceptance/qunit-dom-test.js
@@ -4,7 +4,7 @@ import moduleForAcceptance from '../../tests/helpers/module-for-acceptance';
 moduleForAcceptance('Acceptance | qunit-dom');
 
 test('qunit-dom assertions are available', function(assert) {
-  assert.expect(12);
+  assert.expect(13);
 
   assert.ok(assert.dom, 'assert.dom is available');
   assert.ok(assert.dom('.foo').includesText, 'assert.dom(...).includesText is available');
@@ -19,12 +19,15 @@ test('qunit-dom assertions are available', function(assert) {
     assert.dom('#subtitle').doesNotExist();
     assert.dom('#title').hasText('Welcome to Ember');
 
-    // JSDom based tests aren't able to discern visibility as we define it. Specifically,
-    // the JSDom tests don't do layouting, therefore calculating `offsetWdith` or `offsetHeight`
-    // won't work. As a result, we need to use Ember's test infrastructure to correctly assess
-    // visibility, as those tests run in a browser environment.
+    /*
+     * JSDom based tests aren't able to discern visibility as we define it. Specifically,
+     * the JSDom tests don't do layouting, therefore calculating `offsetWdith` or `offsetHeight`
+     * won't work. As a result, we need to use Ember's test infrastructure to correctly assess
+     * visibility, as those tests run in a browser environment.
+     */
     assert.dom('#title').isVisible();
     assert.dom('#display-block').isVisible();
+    assert.dom('#missing').isNotVisible();
     assert.dom('#display-none').isNotVisible();
     assert.dom('#display-descendant').isNotVisible();
     assert.dom('#hidden-input').isNotVisible();
