ca87cd880e2c1c55225c6fc42eb41c55b424acb5
Tobias Bieniek <tobias.bieniek@gmail.com>
2018-12-10 12:24:15 +0100

    helpers/node-list: Add documentation

--- lib/helpers/node-list.ts
+++ lib/helpers/node-list.ts
@@ -1,3 +1,7 @@
+/**
+ * This function can be used to convert a NodeList to a regular array.
+ * We should be using `Array.from()` for this, but IE11 doesn't support that :(
+ */
 export function toArray(list: NodeList): Node[] {
   return Array.prototype.slice.call(list);
 }
