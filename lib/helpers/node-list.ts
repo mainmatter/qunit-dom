caeb03cc09c4c85918dee66c71289328c668bca6
Tobias Bieniek <tobias.bieniek@gmail.com>
2019-01-04 13:35:27 +0100

    helpers/node-list: Add missing `@private` declaration
    This helper function should not appear in the API docs

--- lib/helpers/node-list.ts
+++ lib/helpers/node-list.ts
@@ -6,6 +6,8 @@ export function toArray(list: NodeList): Node[];
 /**
  * This function can be used to convert a NodeList to a regular array.
  * We should be using `Array.from()` for this, but IE11 doesn't support that :(
+ *
+ * @private
  */
 export function toArray(list: NodeList): Node[] {
   return Array.prototype.slice.call(list);
