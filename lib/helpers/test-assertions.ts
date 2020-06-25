2c0bd6fcb6af24e6179e2e51064aff06f62f05a1
Patsy Issa <patsy.issa@gmail.com>
2020-06-25 20:53:17 +0200

    Duplicate logic for jest tests

--- lib/helpers/test-assertions.ts
+++ lib/helpers/test-assertions.ts
@@ -4,6 +4,10 @@ export default class TestAssertions {
   public results: AssertionResult[] = [];
 
   dom(target: string | Element | null, rootElement?: Element) {
+    if (arguments.length > 0 && arguments[0] === null) {
+      throw new Error('Null target or element was passed');
+    }
+
     return new DOMAssertions(target, rootElement || document, this as any);
   }
 
