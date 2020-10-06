a387f53066809ab8d1ed4317ae6d12eeedc3fb94
Robert Jackson <rjackson@linkedin.com>
2020-10-06 19:20:24 -0400

    Add setup option for #ember-testing override.

--- lib/qunit-dom-modules.ts
+++ lib/qunit-dom-modules.ts
@@ -1,10 +1,20 @@
 import * as QUnit from 'qunit';
 import attach from './install';
+import { overrideRootElement } from './root-element';
 
 export { default as attach } from './install';
 
-interface SetupOptions {}
+interface SetupOptions {
+  getRootElement?: () => Element | null;
+}
 
 export function setup(options: SetupOptions) {
   attach(QUnit.assert);
+
+  const getRootElement =
+    typeof options.getRootElement === 'function'
+      ? options.getRootElement
+      : () => document.querySelector('#ember-testing');
+
+  overrideRootElement(getRootElement);
 }
