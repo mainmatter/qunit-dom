e073fbda3b84014cf019903cdb24fe550f107351
Robert Jackson <rjackson@linkedin.com>
2020-10-21 09:23:34 -0400

    Add comments RE: rationale for logic in treeForAddonTestSupport

--- index.js
+++ index.js
@@ -43,10 +43,19 @@ module.exports = {
         },
       });
     } else {
+      // this differs from ember-cli's default treeForAddonTestSupport in that it
+      // makes our test support modules available at `qunit-dom` **not**
+      // `qunit-dom/test-suppprt`
       let scopedInputTree = new Funnel(`${__dirname}/dist/addon-test-support`, {
         destDir: 'qunit-dom',
       });
 
+      // in order to take advantage of ember-auto-import, we **must** use
+      // `preprocessJs` (because it instruments JS files looking for imports
+      // via the preprocessor registry)
+      //
+      // this also properly transpiles our files based on the consuming applications
+      // targets (through ember-cli-babel)
       return this.preprocessJs(scopedInputTree, '/', this.name, {
         annotation: `qunit-dom - treeForAddonTestSupport`,
         registry: this.registry,
