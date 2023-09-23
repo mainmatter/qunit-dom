fd6913870b61655712c02d67a6e14cd206c8e106
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-09-23 17:04:41 -0400

    npx ember-addon-migrator extract-tests --addon-location qunit-dom --in-place
    Restore workflows, unchanged
    
    Put some documentation config / files back and get 'docs' building working
    
    Looks like all of ci.yml should work now
    
    Cleanup and get tests passing
    
    Add lint:fix script
    
    Get lints passing
    
    Move renovate config back out to the root
    
    release.yml should be up-to-date now as well
    
    Restore ember-try config
    
    Remove test:ember from the addon, because the test app has been extracted
    
    Update ci.yml for compatibility testing
    
    Move projects in to packages folder

---
+++ packages/test-app/.template-lintrc.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = {
+  extends: 'recommended',
+  rules: {
+    'no-forbidden-elements': 'off',
+    'no-inline-styles': 'off',
+    'no-unsupported-role-attributes': 'off',
+  },
+};
