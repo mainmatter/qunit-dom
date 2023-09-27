8a74853c85c35f8804a33cd8dc366bb1508b35c6
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-09-27 12:43:37 -0400

    Drop support for ember-qunit v4 + v5

--- tests/dummy/config/ember-try.js
+++ tests/dummy/config/ember-try.js
@@ -6,32 +6,18 @@ module.exports = async function () {
     usePnpm: true,
     scenarios: [
       {
-        // this is currently the version in package.json, so no dependency changes are needed
-        name: 'ember-qunit-4',
-        npm: {
-          devDependencies: {
-            'ember-qunit': '^4.0.0',
-            'ember-source': '~3.28.0',
-            '@ember/test-helpers': null,
-            qunit: null,
-          },
-        },
-      },
-      {
-        name: 'ember-qunit-5',
-        npm: {
-          devDependencies: {
-            'ember-qunit': '^5.0.0-beta.4',
-            '@ember/test-helpers': '^2.0.0-beta.6',
-            qunit: '^2.11.0',
-          },
-        },
-      },
-      {
         name: 'ember-lts-3.28',
         npm: {
           devDependencies: {
             'ember-source': '~3.28.0',
+          },
+        },
+      },
+      {
+        name: 'ember-lts-4.12',
+        npm: {
+          devDependencies: {
+            'ember-source': '~4.12.0',
           },
         },
       },
