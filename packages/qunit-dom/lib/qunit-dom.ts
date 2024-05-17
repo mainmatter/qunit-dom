25b75e1bf9b8a48c70157b4ff1038096aa263608
BobrImperator <bartholomew.dudzik@gmail.com>
2024-05-17 19:24:11 +0200

    refactor: use common type for the target

--- packages/qunit-dom/lib/qunit-dom.ts
+++ packages/qunit-dom/lib/qunit-dom.ts
@@ -4,4 +4,16 @@ import install from './install.js';
 export { setup } from './qunit-dom-modules.js';
 export { default, DOMAssertionsHandler, type AssertionHandler } from './assertions.js';
 
+import DOMAssertions from './assertions.js';
+
+declare global {
+  type QUnitDOMAssertTarget = string | Element | null;
+  type RootElement = Element | Document | ShadowRoot | null;
+
+  // overwrite the global QUnit interface
+  interface Assert {
+    dom(target?: QUnitDOMAssertTarget, rootElement?: RootElement): DOMAssertions;
+  }
+}
+
 install(QUnit.assert);
